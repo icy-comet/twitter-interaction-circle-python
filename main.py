299944ab2ef116fe2b7221d29a76c72748ee233f
Aniket Teredesai <aniket.teredesai@gmail.com>
2021-05-13 20:59:59 +0530

    improve logic

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
+# all imports
 import os, math, urllib.request
 import tweepy
 from dotenv import load_dotenv
 from PIL import Image, ImageDraw
 import numpy as np
 
+# username to create interaction circle for
+screen_name = 'AniketTeredesai'
+
+# load environment variables
 load_dotenv()
 APP_KEY = os.environ['API_KEY']
 APP_SECRET = os.environ['API_SECRET_KEY']
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
-screen_name = 'AniketTeredesai'
 
+# setup connection
 auth = tweepy.OAuthHandler(APP_KEY, APP_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
+# urllib setup (prevention)
 opener=urllib.request.build_opener()
 opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
 urllib.request.install_opener(opener)
@@ -22,7 +28,7 @@ urllib.request.install_opener(opener)
 
 def get_timeline(screen_name):
     res = []
-    for page in tweepy.Cursor(api.user_timeline, screen_name=screen_name, count=200).pages():
+    for page in tweepy.Cursor(api.user_timeline, screen_name=screen_name, count=200).pages(14):
         for tweet in page:
             res.append(tweet)
     print('Fetched the entire timeline')
@@ -30,7 +36,7 @@ def get_timeline(screen_name):
 
 def get_liked(screen_name):
     res = []
-    for page in tweepy.Cursor(api.favorites, screen_name=screen_name, count=200).pages():
+    for page in tweepy.Cursor(api.favorites, screen_name=screen_name, count=200).pages(14):
         for each_like in page:
             res.append(each_like)
     print('Fetched all likes')
@@ -42,35 +48,33 @@ def select_users(screen_name):
 
     user_scores = {}
 
-    print('starting calculating replies and retweets')
     for tweet in timeline:
-        if tweet.in_reply_to_user_id_str and tweet.in_reply_to_screen_name != screen_name:
+        if tweet.in_reply_to_screen_name and tweet.in_reply_to_screen_name != screen_name:
             if tweet.in_reply_to_screen_name in user_scores.keys():
                 user_scores[tweet.in_reply_to_screen_name] +=4
             else:
                 user_scores.update({tweet.in_reply_to_screen_name : 4})
-            try:
-                if tweet.retweeted_status.user.screen_name != screen_name:
-                    if tweet.retweeted_status.user.screen_name in user_scores.keys():
-                        user_scores[tweet.retweeted_status.user.screen_name] +=6
-                    else:
-                        user_scores.update({tweet.retweeted_status.user.screen_name : 6})
-            except:
-                pass
+        if hasattr(tweet, 'retweeted_status') and tweet.retweeted_status.user.screen_name != screen_name:
+            if tweet.retweeted_status.user.screen_name in user_scores.keys():
+                user_scores[tweet.retweeted_status.user.screen_name] +=6
+            else:
+                user_scores.update({tweet.retweeted_status.user.screen_name : 6})
+    print('calculated replies and retweets')
 
-    print('starting calculating likes')
     for each_like in likes:
         if each_like.user.screen_name != screen_name:
             if each_like.user.screen_name in user_scores.keys():
                 user_scores[each_like.user.screen_name] += 2
             else:
                 user_scores.update({each_like.user.screen_name : 2})
+    print('calculated likes')
 
     sorted_dict = dict(sorted(user_scores.items(), key=lambda x:x[1]))
     pairs = list(sorted_dict.items())
     selected_users = []
     for i in range(len(pairs)-1, len(pairs)-50, -1):
         selected_users.append(pairs[i])
+    print('sorted dict according to the score')
     return dict(selected_users)
 
 def get_avatars(selected_users):
@@ -79,9 +83,10 @@ def get_avatars(selected_users):
     avatars = {}
     for user in res:
         avatars.update({user.screen_name : user.profile_image_url_https.replace('normal', '400x400')})
+    print('fetched all avatar urls')
     return avatars
 
-def get_user_avatar():
+def get_center_avatar():
     user = api.get_user(screen_name=screen_name)
     avatar_url = user.profile_image_url_https.replace('normal', '400x400')
     file, _ = urllib.request.urlretrieve(avatar_url, 'center.jpg')
@@ -93,6 +98,7 @@ def add_avatars(selected_users, avatars):
             'score' : selected_users[user],
             'avatar' : avatars[user]
         }
+    print('avatar urls added to the selected users dictionary')
     return selected_users
 
 def create_bg():
@@ -113,14 +119,15 @@ def create_mask(image):
 
 def create_image(selected_users):
     bg = create_bg()
+    print('created background')
     gap = 10
     layers= [(8, 150, 0, 8), (15, 270, 8, 23), (26, 380, 23, 60)]
-    file = get_user_avatar()
+    file = get_center_avatar()
     bg_w, bg_h = bg.size
     center_avatar = Image.open(file)
     center_avatar = center_avatar.resize((160, 160))
     bg.paste(center_avatar, ((bg_w - 160)//2, (bg_h - 160)//2), create_mask(center_avatar))
-
+    print('pasted central avatar')
     for layer in layers:
         image_count = layer[0]
         gaps_count = image_count-1
@@ -145,6 +152,7 @@ def create_image(selected_users):
             else:
                 pass
     bg.save(f'{screen_name}.jpg')
+    print('image created and saved')
 
 selected_users = select_users(screen_name)
 avatars = get_avatars(selected_users)
