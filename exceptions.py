8dbe68201d601abebe100405a0694e9161bb43c6
Aniket Teredesai <aniket.teredesai@gmail.com>
2022-06-30 14:25:11 +0530

    format with black

--- exceptions.py
+++ exceptions.py
@@ -5,9 +5,11 @@ class InvalidUser(Exception):
     def __str__(self):
         return f"{self.username} isn't a valid username."
 
+
 class InactiveUser(Exception):
     def __str__(self):
         return f"You do not have enough activity on your Twitter profile."
+
 
 class ApiError(Exception):
     def __str__(self):
--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import json
 from image_creation import create_image
 from data_collection import get_data
+
 # from encoding import encode
 import exceptions
 
-screen_name = 'AniketTeredesai' #twitter username
+screen_name = "AniketTeredesai"  # twitter username
 
-layers_config= [{'radius': 150, 'starting_index': 0, 'ending_index': 8},
-                {'radius': 270, 'starting_index': 8, 'ending_index': 23},
-                {'radius': 380, 'starting_index': 23, 'ending_index': 60}]
+layers_config = [
+    {"radius": 150, "starting_index": 0, "ending_index": 8},
+    {"radius": 270, "starting_index": 8, "ending_index": 23},
+    {"radius": 380, "starting_index": 23, "ending_index": 60},
+]
 
-background_color = '#2978b5' #background color of the image produced
+background_color = "#2978b5"  # background color of the image produced
 
-pages_to_fetch = 3 #each page consists of 200 tweets and 20 likes
+pages_to_fetch = 3  # each page consists of 200 tweets and 20 likes
+
 
 def start(screen_name, pages, color, layers_config):
     try:
-        user_avatar_url, layers_config, users_in_circles = get_data(screen_name, pages, layers_config) #unpacking
+        user_avatar_url, layers_config, users_in_circles = get_data(
+            screen_name, pages, layers_config
+        )  # unpacking
         image = create_image(user_avatar_url, color, layers_config)
-        with open(f'{screen_name}_circles.json', 'w') as f:
+        with open(f"{screen_name}_circles.json", "w") as f:
             json.dump(users_in_circles, f)
-        print('users list saved as json')
-        image.save(f'{screen_name}_interaction_circle.jpg')
+        print("users list saved as json")
+        image.save(f"{screen_name}_interaction_circle.jpg")
         # base64_image = encode(image)
     except (exceptions.InactiveUser, exceptions.InvalidUser, exceptions.ApiError) as e:
         print(e)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start(screen_name, pages_to_fetch, background_color, layers_config)
